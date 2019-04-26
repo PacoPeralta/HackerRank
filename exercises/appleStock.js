2d09125e95d3e1d3cf65600e8ef3df6b7096ebaf
Francisco Peralta <franciscoperalta@PacoMacMini.local>
2019-04-25 22:36:24 -0700

    update the apple stock exercise

--- exercises/appleStock.js
+++ exercises/appleStock.js
@@ -2,22 +2,27 @@
 Problem: Find the best profit time to buy-sell a stock of apples.
 The value of each element in the array represents the price and the index represents the time.
 */
-const stock_prices = [10, 7, 5, 8, 11, 9, 12, 16, 4, 2, 3];
+// const stock_prices = [10, 7, 5, 8, 11, 9, 12, 16, 4, 2, 3];
+const stock_prices = [10,8,7,4,-1];
 
 function appleStock() {
-    let stock_prices_cloned = [...stock_prices];
-    while(stock_prices_cloned.length) {
-        let getMinPrice = Math.min(...stock_prices_cloned);
-        let getMaxPrice = Math.max(...stock_prices_cloned);
-        let getMinPriceIndex = stock_prices_cloned.indexOf(getMinPrice);
-        let getMaxPriceIndex = stock_prices_cloned.indexOf(Math.max(...stock_prices_cloned));
+    // Clone the original array for immutability.
+    let cloned_stock_prices = [...stock_prices];
 
-        if(getMinPriceIndex < getMaxPriceIndex) {
-            return getMaxPrice - getMinPrice;
-        } else {
-            stock_prices_cloned = stock_prices_cloned.filter(prices => prices !== getMinPrice);
-        }
+    let profit_balance = [];
+    while (cloned_stock_prices.length > 1) {
+        let balance = {
+            buyTime: 0,
+            sellTime: 0,
+            profit: 0
+        };
+        balance.buyTime = cloned_stock_prices.shift();
+        balance.sellTime = Math.max(...cloned_stock_prices);
+        balance.profit = balance.sellTime - balance.buyTime;
+        profit_balance.push(balance);
     }
+    const getMaxProfit = profit_balance.reduce((prev, current) => (prev.profit > current.profit) ? prev : current);
+    return `${getMaxProfit.profit} (buying for $${getMaxProfit.buyTime} and selling for $${getMaxProfit.sellTime})`;
 }
 
 export default appleStock;
