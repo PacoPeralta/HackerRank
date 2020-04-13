fc6cd37388a8fc186eca8843022ca183f8486000
Francisco Peralta <franciscoperalta@PacoMacMini.attlocal.net>
2020-04-13 13:38:27 -0700

    code improvement to receive random contiguous numbers

--- exercises/minimumSwaps.js
+++ exercises/minimumSwaps.js
@@ -2,8 +2,8 @@ const input = [2, 3, 4, 1, 5];
 
 function swapArray() {
     let swapNumber = 0;
+    let targetNumber = Math.min(...input);
     for (let i = 0; i < input.length; i += 1) {
-        const targetNumber = i + 1;
         if (input[i] !== targetNumber) {
             const currentValue = input[i];
             const getRightIndex = input.indexOf(targetNumber);
@@ -11,6 +11,7 @@ function swapArray() {
             input[getRightIndex] = currentValue;
             swapNumber = swapNumber += 1;
         }
+        targetNumber = targetNumber += 1;
     }
     return swapNumber;
 }
