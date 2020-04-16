d91863a64160f0e8c506237d867b97344f52261c
Paco Peralta <pacoperalta@Pacos-Air.attlocal.net>
2020-04-16 14:21:14 -0700

    test cases added to appleStock exercise

--- exercises/appleStock/__tests__/appleStock.test.js
+++ exercises/appleStock/__tests__/appleStock.test.js
@@ -1,5 +1,35 @@
 import appleStock from "../appleStock";
 
-it('should return the result', function () {
-    expect(appleStock()).toEqual( "-1 (buying for $8 and selling for $7)")
+// Output shape: {"buyTime": 8, "profit": -1, "sellTime": 7}
+// Default input: [10, 7, 5, 8, 11, 9, 12, 16, 4, 2, 3]
+
+test('should return the default result', function () {
+    expect(appleStock()).toEqual({
+        buyTime: 5,
+        profit: 11,
+        sellTime: 16
+    });
+});
+
+test('should return result for negative values', function () {
+    const negativeValues = [-7, -5, -3, -14, -2];
+    expect(appleStock(negativeValues)).toEqual({
+        buyTime: -14,
+        profit: 12,
+        sellTime: -2
+    });
+});
+
+test('should return the minimal lost', function () {
+    const negativeValues = [25, 18, 12, 10, 4, 3];
+    expect(appleStock(negativeValues)).toEqual({
+        buyTime: 4,
+        profit: -1,
+        sellTime: 3
+    });
+});
+
+test('profit should be same as sellTime - buyTime', function () {
+    const {buyTime, profit, sellTime} = appleStock();
+    expect(profit).toEqual(sellTime - buyTime);
 });
--- exercises/appleStock/appleStock.js
+++ exercises/appleStock/appleStock.js
@@ -3,11 +3,11 @@ Problem: Find the best profit time to buy-sell a stock of apples.
 The value of each element in the array represents the price and the index represents the time.
 */
 // const stock_prices = [10, 7, 5, 8, 11, 9, 12, 16, 4, 2, 3];
-const stock_prices = [10,8,7,4,-1];
+const stock_prices = [10, 7, 5, 8, 11, 9, 12, 16, 4, 2, 3];
 
-function appleStock() {
+function appleStock(input = stock_prices) {
     // Clone the original array for immutability.
-    let cloned_stock_prices = [...stock_prices];
+    let cloned_stock_prices = [...input];
 
     let profit_balance = [];
     while (cloned_stock_prices.length > 1) {
@@ -21,8 +21,7 @@ function appleStock() {
         balance.profit = balance.sellTime - balance.buyTime;
         profit_balance.push(balance);
     }
-    const getMaxProfit = profit_balance.reduce((prev, current) => (prev.profit > current.profit) ? prev : current);
-    return `${getMaxProfit.profit} (buying for $${getMaxProfit.buyTime} and selling for $${getMaxProfit.sellTime})`;
+    return profit_balance.reduce((prev, current) => (prev.profit > current.profit) ? prev : current);
 }
 
 export default appleStock;
